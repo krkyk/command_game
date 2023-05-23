6b819bb4487714b09b4ecc548b801e7d6a647135
krkyk <100661771+krkyk@users.noreply.github.com>
2023-05-23 12:00:56 +0900

    こうげきフェーズの仮作成

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,80 @@ import { Hero } from "./components/Hero/Hero";
 import { Message } from "./components/Message/Message";
 import { Monster } from "./components/Monster/Monster";
 import "./styles.css";
+import { wait } from "./utils/wait";
 
 export default function App() {
   const [hp, setHp] = useState(100);
   const [monsterHp, setMonsterHp] = useState(100);
   const [message, setMessage] = useState(null);
+  const [BattleStatus, setBattleStatus] = useState();
   const [isEscaped, setIsEscaped] = useState(false);
   const [isTransformed, setIsTransformed] = useState(false);
   const [transformMessage, setTransformMessage] = useState(null);
+  const ATTACK_POINT = {
+    MAX: 31,
+    MIN: 10
+  };
+  const BATTLE_STATUS = {
+    DOING: 0,
+    END: 1
+  };
 
   const handleAttack = () => {
-    setMonsterHp(monsterHp - 10);
+    Promise.resolve()
+      .then(() => handleHeroAttack())
+      .then(() => wait(2000))
+      .then(() => handleMonsterAttack())
+      .then(() => wait(2000))
+      .then(() => handleHeroAttack())
+      .then(() => wait(2000))
+      .then(() => handleMonsterAttack())
+      .then(() => wait(2000))
+      .then(() => handleHeroAttack())
+      .then(() => wait(2000))
+      .then(() => handleMonsterAttack())
+      .then(() => wait(2000))
+      .catch((err) => handleError(err));
+
+    const getAttackPoint = () => {
+      const min = Math.ceil(ATTACK_POINT.MIN);
+      const max = Math.floor(ATTACK_POINT.MAX);
+      return Math.floor(Math.random() * (max - min) + min);
+    };
+
+    const handleHeroAttack = () => {
+      setMessage("忍者の攻撃！");
+      const attackPoint = getAttackPoint();
+      setMonsterHp(monsterHp - attackPoint);
+      if (monsterHp < 0) {
+        throw new Error("MONSTER_DEAD");
+      }
+    };
+
+    const handleMonsterAttack = () => {
+      setMessage("モンスターの攻撃！");
+      const attackPoint = getAttackPoint();
+      setHp(hp - attackPoint);
+      if (hp < 0) {
+        throw new Error("HERO_DEAD");
+      }
+    };
+
+    const handleError = (err) => {
+      switch (err.message) {
+        case "MONSTER_DEAD":
+          setMessage("モンスターをたおした！");
+          setBattleStatus(BATTLE_STATUS.END);
+          break;
+        case "HERO_DEAD":
+          setMessage("忍者はたおれてしまった！");
+          setBattleStatus(BATTLE_STATUS.END);
+          break;
+        default:
+          alert(err);
+          break;
+      }
+    };
   };
 
   const handleEscape = () => {
@@ -38,7 +101,9 @@ export default function App() {
         handleEscape={handleEscape}
         handleTransform={handleTransform}
       />
-      <button className="reset-btn">全てリセット</button>
+      <button className="reset-btn" onClick={() => window.location.reload()}>
+        全てリセット
+      </button>
     </div>
   );
 }
--- src/components/CommandBox/CommandBox.jsx
+++ src/components/CommandBox/CommandBox.jsx
@@ -3,9 +3,9 @@ import "./CommandBox.css";
 export const CommandBox = ({ handleAttack, handleEscape, handleTransform }) => {
   return (
     <div className="box">
-      <button onClick={handleAttack}>攻撃</button>
-      <button onClick={handleEscape}>逃げる</button>
-      <button onClick={handleTransform}>変身</button>
+      <button onClick={handleAttack}>たたかう</button>
+      <button onClick={handleEscape}>にげる</button>
+      <button onClick={handleTransform}>へんしん</button>
     </div>
   );
 };
